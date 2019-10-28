fb70d02eb702e7363fb2361bd073c2bd338cd241
Elkin Jimenez <elomjiga@hotmail.com>
2019-10-28 11:48:26 -0500

    Las funciones del index fueron puestas en otro archivo .js

---
+++ js/funciones.js
@@ -0,0 +1,15 @@
+function animar() {
+    new WOW().init();
+}
+
+function contactame() {
+    $("#contenido_pagina").load("contact.html");
+}
+
+function aboutme() {
+    $("#contenido_pagina").load("about.html");
+}
+
+function portafolio() {
+    $("#contenido_pagina").load("portafolio.html");
+}
