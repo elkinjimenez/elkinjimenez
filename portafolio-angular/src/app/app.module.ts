26304d3f6c7f232e4b2a3cb702f90c9589c4ebb2
elkinjimenez <elkin.jiga@hotmail.com>
2023-01-13 09:30:43 -0500

    Organizando linea base de root.

--- portafolio-angular/src/app/app.module.ts
+++ portafolio-angular/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AboutComponent } from './modules/about/about.component';
+import { AppComponent } from './modules/root/app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
