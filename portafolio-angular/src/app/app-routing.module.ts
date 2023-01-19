4b316914b993b3cd2ba91c29d7e4550384657206
elkinjimenez <elkin.jiga@hotmail.com>
2023-01-18 19:19:17 -0500

    Agregando seccion de portfolio al menu y routing.

--- portafolio-angular/src/app/app-routing.module.ts
+++ portafolio-angular/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './modules/home/home.component';
 import { AboutComponent } from './modules/about/about.component';
+import { PortfolioComponent } from './modules/portfolio/portfolio.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'aboutme', component: AboutComponent },
+  { path: 'portfolio', component: PortfolioComponent },
 ];
 
 @NgModule({
