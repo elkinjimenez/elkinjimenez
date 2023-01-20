a0eb78c7b23d848353841de18a871193b77be69f
elkinjimenez <elkin.jiga@hotmail.com>
2023-01-19 20:43:58 -0500

    Llamada de componente de studies.

--- portafolio-angular/src/app/app-routing.module.ts
+++ portafolio-angular/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { AboutComponent } from './modules/about/about.component';
 import { PortfolioComponent } from './modules/portfolio/portfolio.component';
+import { StudiesComponent } from './modules/studies/studies.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'aboutme', component: AboutComponent },
   { path: 'portfolio', component: PortfolioComponent },
+  { path: 'studies', component: StudiesComponent },
 ];
 
 @NgModule({
