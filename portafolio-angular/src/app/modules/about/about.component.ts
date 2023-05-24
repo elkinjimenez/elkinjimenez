6142a94e61b2253dde4670e35cc2166b5924a5c1
elkinjimenez <elkin.jiga@hotmail.com>
2023-05-23 20:18:22 -0500

    Creando Chart para Java.

--- portafolio-angular/src/app/modules/about/about.component.ts
+++ portafolio-angular/src/app/modules/about/about.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+
+  @ViewChild('java') javaCanvas!: ElementRef;
+
+  ngAfterViewInit() {
+    this.donaJava();
+  }
+
+  private donaJava() {
+    const javaCtx = this.javaCanvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+    const myjava = new Chart(javaCtx, {
+      type: 'doughnut',
+      data: {
+        labels: ['Conocimiento al', 'Por mejorar'],
+        datasets: [{
+          data: [80, 20],
+          backgroundColor: ["#304ffe", "#212121"],
+          hoverBackgroundColor: ["#0023eb", "#212121"]
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false
+          },
+        }
+      }
+    });
+  }
 
 }
