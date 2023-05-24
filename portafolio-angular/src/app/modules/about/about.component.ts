8c2d257644c09de4c2150cb5a7d12a9d764b283f
elkinjimenez <elkin.jiga@hotmail.com>
2023-05-23 20:47:18 -0500

    Creando los demas Charts.

--- portafolio-angular/src/app/modules/about/about.component.ts
+++ portafolio-angular/src/app/modules/about/about.component.ts
@@ -10,32 +10,82 @@ import Chart from 'chart.js/auto';
 export class AboutComponent {
 
   @ViewChild('java') javaCanvas!: ElementRef;
+  @ViewChild('javaScript') javaScriptCanvas!: ElementRef;
+  @ViewChild('html') htmlCanvas!: ElementRef;
+  @ViewChild('ux') uxCanvas!: ElementRef;
+  @ViewChild('ui') uiCanvas!: ElementRef;
+  @ViewChild('db') dbCanvas!: ElementRef;
+
+  private labels = ['Conocimiento al', 'Por mejorar'];
 
   ngAfterViewInit() {
-    this.donaJava();
+    this.printCharts();
   }
 
-  private donaJava() {
-    const javaCtx = this.javaCanvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
-    const myjava = new Chart(javaCtx, {
-      type: 'doughnut',
-      data: {
-        labels: ['Conocimiento al', 'Por mejorar'],
-        datasets: [{
-          data: [80, 20],
-          backgroundColor: ["#304ffe", "#212121"],
-          hoverBackgroundColor: ["#0023eb", "#212121"]
-        }]
+  private printCharts() {
+    const charts: MyChart[] = [
+      {
+        canvas: this.javaCanvas,
+        data: [80, 20],
+        backgroundColor: ["#304ffe", "#212121"],
+        hoverBackgroundColor: ["#0023eb", "#212121"]
       },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: {
-            display: false
-          },
-        }
+      {
+        canvas: this.javaScriptCanvas,
+        data: [72, 28],
+        backgroundColor: ["#ffff00", "#212121"],
+        hoverBackgroundColor: ["#dede23", "#212121"]
+      },
+      {
+        canvas: this.htmlCanvas,
+        data: [95, 5],
+        backgroundColor: ["#00C851", "#212121"],
+        hoverBackgroundColor: ["#068c3c", "#212121"]
+      },
+      {
+        canvas: this.uxCanvas,
+        data: [90, 10],
+        backgroundColor: ["#2BBBAD", "#212121"],
+        hoverBackgroundColor: ["#209186", "#212121"]
+      },
+      {
+        canvas: this.uiCanvas,
+        data: [93, 7],
+        backgroundColor: ["#d50000", "#212121"],
+        hoverBackgroundColor: ["#b71c1c", "#212121"]
+      },
+      {
+        canvas: this.dbCanvas,
+        data: [75, 25],
+        backgroundColor: ["#aa00ff", "#212121"],
+        hoverBackgroundColor: ["#4a148c", "#212121"]
       }
-    });
+    ];
+    charts.forEach(chart => {
+      const dona = chart.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+      const myDona = new Chart(dona, {
+        type: 'doughnut',
+        data: {
+          labels: this.labels,
+          datasets: [chart]
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              display: false
+            },
+          }
+        }
+      });
+    })
   }
 
+}
+
+interface MyChart {
+  canvas: ElementRef;
+  data: number[];
+  backgroundColor: string[];
+  hoverBackgroundColor: string[];
 }
